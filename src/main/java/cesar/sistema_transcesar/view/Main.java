2f2824df6cb3c36dd1d8189a554ff333c8c9b117
kstrodeveloper <aynerjosec@gmail.com>
2026-03-12 20:49:25 -0500

    feat: implementar menu principal de consola con todas las opciones

--- src/main/java/cesar/sistema_transcesar/view/Main.java
+++ src/main/java/cesar/sistema_transcesar/view/Main.java
@@ -1,5 +1,57 @@
 package cesar.sistema_transcesar.view;
 
+import java.util.Scanner;
+
 public class Main {
-    
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int opcion;
+        do {
+            mostrarMenu();
+            opcion = sc.nextInt();
+            sc.nextLine();
+            switch (opcion) {
+                case 1: registrarVehiculo(); break;
+                case 2: listarVehiculos(); break;
+                case 3: registrarConductor(); break;
+                case 4: registrarPasajero(); break;
+                case 5: venderTicket(); break;
+                case 6: verEstadisticas(); break;
+                case 0: System.out.println("Saliendo del sistema..."); break;
+                default: System.out.println("Opcion no valida.");
+            }
+        } while (opcion != 0);
+    }
+
+    static void mostrarMenu() {
+        System.out.println("\n====== TransCesar S.A.S. ======");
+        System.out.println("1. Registrar vehiculo");
+        System.out.println("2. Listar vehiculos");
+        System.out.println("3. Registrar conductor");
+        System.out.println("4. Registrar pasajero");
+        System.out.println("5. Vender ticket");
+        System.out.println("6. Ver estadisticas");
+        System.out.println("0. Salir");
+        System.out.print("Selecciona una opcion: ");
+    }
+
+    // TODO: Actividad 3
+    static void registrarVehiculo() {}
+
+    // TODO: Actividad 3
+    static void listarVehiculos() {}
+
+    // TODO: Actividad 4
+    static void registrarConductor() {}
+
+    // TODO: Actividad 5
+    static void registrarPasajero() {}
+
+    // TODO: Actividad 6
+    static void venderTicket() {}
+
+    // TODO: Actividad 12
+    static void verEstadisticas() {}
 }
