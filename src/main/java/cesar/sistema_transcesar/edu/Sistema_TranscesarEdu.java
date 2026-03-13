49c6c0bd4948dbb4f29f9a6c1680f6c8a4ee098d
kstrodeveloper <aynerjosec@gmail.com>
2026-03-12 20:34:43 -0500

    feat: inicializar proyecto TransCesar

---
+++ src/main/java/cesar/sistema_transcesar/edu/Sistema_TranscesarEdu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cesar.sistema_transcesar.edu;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Sistema_TranscesarEdu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
