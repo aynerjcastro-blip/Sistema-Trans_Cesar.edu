4da282fdfff8225330704824b25cbeaf557fd2c1
Jose David De Jesus Reales Montero <jdaviddejesusreales@unicesar.edu.co>
2026-03-14 21:12:18 -0500

    feat: crear clase abstracta Vehiculo con encapsulamiento e Imprimible

---
+++ src/main/java/cesar/sistema_transcesar/model/vehiculos/Vehiculo.java
@@ -0,0 +1,84 @@
+package cesar.sistema_transcesar.model.vehiculos;
+
+import cesar.sistema_transcesar.model.interfaces.Imprimible;
+
+public abstract class Vehiculo implements Imprimible {
+    private String placa;
+    private String ruta;
+    private int capacidadMaxima;
+    private int pasajerosActuales;
+    private double tarifaBase;
+    private boolean disponible;
+
+    public Vehiculo(String placa, String ruta, int capacidadMaxima, int pasajerosActuales, double tarifaBase, boolean disponible) {
+        this.placa = placa;
+        this.ruta = ruta;
+        this.capacidadMaxima = capacidadMaxima;
+        this.pasajerosActuales = pasajerosActuales;
+        this.tarifaBase = tarifaBase;
+        this.disponible = disponible;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public int getPasajerosActuales() {
+        return pasajerosActuales;
+    }
+
+    public void setPasajerosActuales(int pasajerosActuales) {
+        this.pasajerosActuales = pasajerosActuales;
+    }
+
+    public double getTarifaBase() {
+        return tarifaBase;
+    }
+
+    public void setTarifaBase(double tarifaBase) {
+        this.tarifaBase = tarifaBase;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public int getCuposDisponibles() {
+        return capacidadMaxima - pasajerosActuales;
+    }
+
+    @Override
+    public void imprimirDetalle(){
+        System.out.println("Placa: " + placa
+        + "Ruta: " + ruta
+        + "Capacidad Maxima: " + capacidadMaxima
+        + "Pasajeros Actuales: " + pasajerosActuales
+        + "Cupos disponibles: " + getCuposDisponibles()
+        + "Tarifa Base: " + tarifaBase
+        + "Disponible: " + disponible);
+    }
+}
