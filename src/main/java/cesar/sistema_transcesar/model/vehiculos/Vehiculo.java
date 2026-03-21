92426bb8be731fa350a5e075e80dbaa816860329
Jose David De Jesus Reales Montero <jdaviddejesusreales@unicesar.edu.co>
2026-03-20 20:18:53 -0500

    feat: Agregar sobrecarga de constructores a Vehiculo

--- src/main/java/cesar/sistema_transcesar/model/vehiculos/Vehiculo.java
+++ src/main/java/cesar/sistema_transcesar/model/vehiculos/Vehiculo.java
@@ -22,6 +22,48 @@ public abstract class Vehiculo implements Imprimible {
         this.conductor = conductor;
     }
 
+    public Vehiculo(String placa, String ruta, int capacidadMaxima, int pasajerosActuales, double tarifaBase, boolean disponible) {
+        this.placa = placa;
+        this.ruta = ruta;
+        this.capacidadMaxima = capacidadMaxima;
+        this.pasajerosActuales = pasajerosActuales;
+        this.tarifaBase = tarifaBase;
+        this.disponible = disponible;
+    }
+
+    public Vehiculo(String placa, String ruta, int capacidadMaxima, int pasajerosActuales, double tarifaBase) {
+        this.placa = placa;
+        this.ruta = ruta;
+        this.capacidadMaxima = capacidadMaxima;
+        this.pasajerosActuales = pasajerosActuales;
+        this.tarifaBase = tarifaBase;
+    }
+
+    public Vehiculo(String placa, String ruta, int capacidadMaxima, int pasajerosActuales) {
+        this.placa = placa;
+        this.ruta = ruta;
+        this.capacidadMaxima = capacidadMaxima;
+        this.pasajerosActuales = pasajerosActuales;
+    }
+
+    public Vehiculo(String placa, String ruta, int capacidadMaxima) {
+        this.placa = placa;
+        this.ruta = ruta;
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public Vehiculo(String placa, String ruta) {
+        this.placa = placa;
+        this.ruta = ruta;
+    }
+
+    public Vehiculo(String placa) {
+        this.placa = placa;
+    }
+
+    public Vehiculo() {
+    }
+
     public String getPlaca() {
         return placa;
     }
@@ -84,13 +126,13 @@ public abstract class Vehiculo implements Imprimible {
 
     @Override
     public void imprimirDetalle(){
-        System.out.println("Placa: " + placa
-        + "Ruta: " + ruta
-        + "Capacidad Maxima: " + capacidadMaxima
-        + "Pasajeros Actuales: " + pasajerosActuales
-        + "Cupos disponibles: " + getCuposDisponibles()
-        + "Tarifa Base: " + tarifaBase
-        + "Disponible: " + disponible);
+        System.out.println("\nPlaca: " + placa
+        + "\nRuta: " + ruta
+        + "\nCapacidad Maxima: " + capacidadMaxima
+        + "\nPasajeros Actuales: " + pasajerosActuales
+        + "\nCupos disponibles: " + getCuposDisponibles()
+        + "\nTarifa Base: " + tarifaBase
+        + "\nDisponible: " + disponible);
     }
 
     @Override
