380c537bb7dcbfa4a25c5a034ecaa3e7e30b5a18
Jose David De Jesus Reales Montero <jdaviddejesusreales@unicesar.edu.co>
2026-03-20 19:44:47 -0500

    feat: Agregar parametro disponible al constructor de las subclases vehiculo

--- src/main/java/cesar/sistema_transcesar/model/vehiculos/Buseta.java
+++ src/main/java/cesar/sistema_transcesar/model/vehiculos/Buseta.java
@@ -1,7 +1,7 @@
 package cesar.sistema_transcesar.model.vehiculos;
 
 public class Buseta extends Vehiculo {
-    public Buseta(String placa, String ruta){
+    public Buseta(String placa, String ruta, boolean disponible){
         super(placa,ruta,19,0,8000,true);
     }
 }
--- src/main/java/cesar/sistema_transcesar/model/vehiculos/MicroBus.java
+++ src/main/java/cesar/sistema_transcesar/model/vehiculos/MicroBus.java
@@ -1,7 +1,7 @@
 package cesar.sistema_transcesar.model.vehiculos;
 
 public class MicroBus extends Vehiculo {
-    public MicroBus(String placa, String ruta){
+    public MicroBus(String placa, String ruta, boolean disponible){
         super(placa,ruta,25,0,10000,true);
     }
 }
