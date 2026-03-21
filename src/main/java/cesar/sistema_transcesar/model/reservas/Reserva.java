13d30601d7904a0644f62ad68299cfc3af3d85c6
Jose David De Jesus Reales Montero <jdaviddejesusreales@unicesar.edu.co>
2026-03-21 09:32:17 -0500

    feat: Agregar getters y setters a la clase Reserva

--- src/main/java/cesar/sistema_transcesar/model/reservas/Reserva.java
+++ src/main/java/cesar/sistema_transcesar/model/reservas/Reserva.java
@@ -20,4 +20,52 @@ public class Reserva {
         this.fechaViaje = fechaViaje;
         this.estado = estado;
     }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+
+    public void setPasajero(Pasajero pasajero) {
+        this.pasajero = pasajero;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public LocalDate getFechaViaje() {
+        return fechaViaje;
+    }
+
+    public void setFechaViaje(LocalDate fechaViaje) {
+        this.fechaViaje = fechaViaje;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
